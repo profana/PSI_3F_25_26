84c1b74c57e773f4cb92bbf21fb9f49be9700901
Ana Esteves <11687547+profana@users.noreply.github.com>
2025-10-01 23:36:02 +0100

    1º commit

---
+++ M17C_ex02/M17C_ex02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M17C_ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Segundo programa");
+        }
+    }
+}
