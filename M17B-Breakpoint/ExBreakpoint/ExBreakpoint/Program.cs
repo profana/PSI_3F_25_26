17d6d78a02154f2c3bfdd0fd016713006c82b549
Ana Esteves <11687547+profana@users.noreply.github.com>
2025-12-04 17:45:59 +0000

    primeiro commit

---
+++ M17B-Breakpoint/ExBreakpoint/ExBreakpoint/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int valorA = LerValorA();
+        int valorB = LerValorB();
+
+        int resultado = CalcularResultado(valorA, valorB);
+
+        Console.WriteLine("Resultado: ",resultado);
+    }
+
+    static int LerValorA()
+    {
+        return 10; 
+    }
+
+    static int LerValorB()
+    {
+        return ObterDivisor();
+    }
+
+    static int ObterDivisor()
+    {
+        return 0; 
+    }
+
+    static int CalcularResultado(int x, int y)
+    {
+        return Dividir(x, y);
+    }
+
+    static int Dividir(int x, int y)
+    {
+        // ***** coloca o breakpoint aqui *****
+        return x / y;
+    }
+}
